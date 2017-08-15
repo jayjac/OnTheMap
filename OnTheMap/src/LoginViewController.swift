836c350e55eff3cedf7d3b8402fc1455ed5744f0
jayjac <jy_jac@msn.com>
2017-08-15 14:10:33 +0200

    "Before going on a vacation" commit

--- OnTheMap/src/LoginViewController.swift
+++ OnTheMap/src/LoginViewController.swift
@@ -13,8 +13,33 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        testUdacityParse()
+    }
+    
+    func testUdacityParse() {
+        let url = URL(string: "https://parse.udacity.com/parse/classes/StudentLocation")!
+        let request = NSMutableURLRequest(url: url)
+        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+        let session = URLSession.shared
+        let task = session.dataTask(with: request as URLRequest) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let response = response as? HTTPURLResponse {
+                print(response.statusCode)
+            }
+            if let data = data {
+                do {
+                    if let jsonObject = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as? [String: AnyObject] {
+                        let results = jsonObject["results"] as! [[String: AnyObject]]
+                        print("There were \(results.count) objects found")
+                    }
+                }
+                catch {
+                    
+                }
 
-        // Do any additional setup after loading the view.
+            }
+        }
+        task.resume()
     }
 
     
